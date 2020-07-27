1a8b786c0cefb83391cf594660cb0e409b6f01fd
IMRAN NOOR <imrannoor1992@outlook.com>
2020-07-27 18:07:13 +0500

    jjj

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,51 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import nextCookie from "next-cookies";
-import Router from 'next/router';
-import IntroMenu from "@components/IntroSection/introMenu"
+import Router from "next/router";
+import { initializeStore } from "../../redux/store";
+import IntroMenu from "@components/IntroSection/introMenu";
 
 const LandingPage = (props) => {
   const { user } = useSelector((state) => state["USER"]);
-  console.log(props.isMobileView, 'isMobileView', user);
-  
-  useEffect(() => {
-    const {pathname} = Router
-    let classId = localStorage.getItem("selected-class")
+  //console.log(props.isMobileView, "isMobileView", user);
 
-    if(classId != null ){
-      Router.push(`/online-courses/${classId || user._id}`)
-    }else if(props.isMobileView != null){
-        Router.push('/programs')
+  useEffect(() => {
+    const { pathname } = Router;
+    let classId = localStorage.getItem("selected-class");
+
+    if (classId != null) {
+      Router.push(`/online-courses/${classId || user._id}`);
+    } else if (props.isMobileView != null) {
+      Router.push("/programs");
     }
   });
 
-  return (
-     <IntroMenu apolloClient={null} apolloState={null} />
-  );
+  return <IntroMenu apolloClient={null} apolloState={null} />;
 };
 
 LandingPage.getInitialProps = async (ctx) => {
-  const { user } = ctx.store.getState().USER;
+  const store = initializeStore();
+  const { user } = store.getState().USER;
   const { Authorization } = nextCookie(ctx);
 
   let isMobileView = (ctx.req
-    ? ctx.req.headers['user-agent']
-    : navigator.userAgent).match(
-      /Android|BlackBerry|iPhone|IEMobile/i
-    )
+    ? ctx.req.headers["user-agent"]
+    : navigator.userAgent
+  ).match(/Android|BlackBerry|iPhone|IEMobile/i);
 
-    console.log(isMobileView, 'isMobileView', user);
-    
-    if (ctx.req && Authorization) {
-      if (user && user.role === "student")
-        ctx.res.writeHead(302, { Location: "/online-courses/"+user.classRoom }).end();
-      else ctx.res.writeHead(302, { Location: "/online-class"}).end();
-    } else if (Authorization) {
-      if (user && user.role === "student")
-        document.location.pathname = "/online-courses/"+user.classRoom;
-      else document.location.pathname = "/online-class";
-    } else 
-    return { Authorization, isMobileView };  
+  //console.log(isMobileView, "isMobileView", user);
+
+  if (ctx.req && Authorization) {
+    if (user && user.role === "student")
+      ctx.res
+        .writeHead(302, { Location: "/online-courses/" + user.classRoom })
+        .end();
+    else ctx.res.writeHead(302, { Location: "/online-class" }).end();
+  } else if (Authorization) {
+    if (user && user.role === "student")
+      document.location.pathname = "/online-courses/" + user.classRoom;
+    else document.location.pathname = "/online-class";
+  } else return { Authorization, isMobileView };
 };
-
 
 export default LandingPage;
