372e8112806542c26779b793f2a43e52bc715e97
IMRAN NOOR <imrannoor1992@outlook.com>
2020-08-05 16:07:33 +0500

    SOOO

--- pages/payment/easypaisa/success.js
+++ pages/payment/easypaisa/success.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useRouter } from "next/router";
+import { isEmpty } from "lodash";
 import { siteUrl } from "@utils/utilities";
 const PaymentEasyPaisaEndResult = () => {
   const { query, replace, push } = useRouter();
   const { success, batchNumber, amount, orderRefNumber } = query;
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
+  return !isEmpty(query) ? (
+    <div style={{ width: "100vw", height: "100vh" }} className=''>
       <div>{success ? "Success" : "Failed"}</div>
       <div>{amount}</div>
       <div>{orderRefNumber}</div>
     </div>
+  ) : (
+    <div />
   );
 };
 export default PaymentEasyPaisaEndResult;
