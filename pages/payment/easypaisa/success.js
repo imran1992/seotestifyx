7f45fc37f57d31ccec3bbecfb78d679ea7d34e60
IMRAN NOOR <imrannoor1992@outlook.com>
2020-08-05 16:13:46 +0500

    successQuery

--- pages/payment/easypaisa/success.js
+++ pages/payment/easypaisa/success.js
@@ -4,9 +4,15 @@ import { isEmpty } from "lodash";
 import { siteUrl } from "@utils/utilities";
 const PaymentEasyPaisaEndResult = () => {
   const { query, replace, push } = useRouter();
-  const { success, batchNumber, amount, orderRefNumber } = query;
+  const { success, amount, orderRefNumber, batchNumber } = query;
+  useEffect(() => {
+    !isEmpty(query) && console.log("SuccessQuery", query);
+  }, [query]);
   return !isEmpty(query) ? (
-    <div style={{ width: "100vw", height: "100vh" }} className=''>
+    <div
+      style={{ width: "100vw", height: "100vh", padding: "5vw" }}
+      className=""
+    >
       <div>{success ? "Success" : "Failed"}</div>
       <div>{amount}</div>
       <div>{orderRefNumber}</div>
