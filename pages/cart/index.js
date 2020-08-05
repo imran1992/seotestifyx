271dc9344fb9663655d7df489d4a724dab863729
IMRAN NOOR <imrannoor1992@outlook.com>
2020-08-05 19:32:13 +0500

    SeletAbleCouses

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -118,6 +118,8 @@ const Cart = () => {
                       height: 31,
                       border: "1px solid #28a745",
                       borderRadius: 31 / 2,
+                      boxShadow:
+                        "0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)",
                     }}
                   >
                     <div
@@ -137,6 +139,8 @@ const Cart = () => {
                       height: 31,
                       border: "1px solid gray",
                       borderRadius: 31 / 2,
+                      boxShadow:
+                        "0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)",
                     }}
                   ></div>
                 )}
