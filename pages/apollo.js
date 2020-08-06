48a4c06b596966ff39fefd8c33ed3b5d0528a468
IMRAN NOOR <imrannoor1992@outlook.com>
2020-08-06 15:46:58 +0500

    hmm

--- pages/apollo.js
+++ pages/apollo.js
@@ -1,13 +1,10 @@
-// import Layout from '../components/Layout'
-// import Submit from '../components/Submit'
-import PostList from '../components/PostList'
-import { withApollo } from '../lib/apollo'
+import PostList from "../components/PostList";
+import { withApollo } from "../lib/apollo";
 
 const ApolloPage = () => (
   <div>
-    {/* <Submit /> */}
     <PostList />
   </div>
-)
+);
 
-export default withApollo(ApolloPage)
+export default withApollo(ApolloPage);
